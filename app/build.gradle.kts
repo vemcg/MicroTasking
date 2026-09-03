02f123febba4fa47244ce4f510ebbfed7227643c
Vern McGeorge <VernMcGeorge@gmail.com>
2026-09-02 20:47:13 -0700

    Bump Android version for update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.microtasking.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.1.6"
+        versionCode = 7
+        versionName = "0.1.7"
     }
 
     signingConfigs {
